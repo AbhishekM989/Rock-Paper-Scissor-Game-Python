461cf4d10993dd7e1c0ecc8380290f3ea4dd81a5
AbhishekM989 <abhishekmaurya9f@gmail.com>
2022-04-18 00:31:49 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import random
+
+while True:
+
+    user_action = input("Enter Your Choice (Rock, Paper, Scissor): ")
+
+    possible_actions = ["Rock", "Paper", "Scissor"]
+    computer_action = random.choice(possible_actions)
+
+    print(f"\nYou Chose {user_action}, Computer Chose {computer_action}. \n")
+
+
+    if user_action == computer_action:
+        print(f"Both Players Selected {user_action} It's A Tie!")
+
+    elif user_action == "Rock":
+        if computer_action == "Scissor":
+            print(f"Rock Smashes Scissor ! You Win !")
+
+        else:
+            print(f"Paper Covers Rock You Lose !")
+
+
+    elif user_action == "Paper":
+        if computer_action == "Rock":
+            print(f"Paper Cover Rock You Lose !")
+
+        else:
+            print('Scissor Cuts Paper You Win !')
+
+    elif user_action == "Scissor":
+        if computer_action == "Paper":
+            print("Scissor Cuts Paper You Win !")
+
+        else:
+            print("Rock Smashes Scissor You Lose !")        
